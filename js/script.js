31f5a79c37ee520667c4a9666cfe4b16a97d7dbf
Viacheslav Hanchuk <brainon.by@gmail.com>
2019-01-23 21:02:54 +0300

    added js-function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,51 @@
 $(document).ready(function () {
-    
+    $('.card').mouseover(function () {
+        $(this).find('.thumbnail').stop().fadeIn()
+    });
+
+    $('.card').mouseout(function () {
+        $(this).find('.thumbnail').stop().fadeOut()
+    });
+
+    var accord = $('.accordion');
+    accord.find('.title:not(.active)').siblings('div').slideUp();
+    accord.find('.title').on('click', function () {
+        $(this).siblings('div').stop().slideToggle(500);
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).addClass('active');
+        }
+    })
+
+    var btnUp = $('.up');
+    btnUp.hide();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            btnUp.fadeIn();
+        } else {
+            btnUp.fadeOut();
+        }
+    })
+
+    btnUp.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return falce;
+    })
+
+    var slider = $('.slides');
+    slider.owlCarousel({
+        items: 1,
+        dotsContainer: '.dots',
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true
+    });
+
+    $("#tabs").tabs({
+        active: 0
+    });
 })
